3f82d56812b6026cf79413aa22925a28547e02a4
UnknownProjectCreator <moonanmore@gmail.com>
2026-08-26 17:32:47 +0900

    DownLoad Package

--- 26311048_HongSungChan_TestFile/_main.cpp
+++ 26311048_HongSungChan_TestFile/_main.cpp
@@ -3,8 +3,36 @@
 #include <iostream>
 #include <vector>
 #include <string>
+#include <glc2d.h>
+
+// link the 2d game library
+#if defined(_DEBUG)
+#if defined(_M_X64) // 64-bit 아키텍처
+#pragma comment(lib, "glc2d_x64_debug.lib")
+#elif defined(_M_IX86) // 32-bit 아키텍처
+#pragma comment(lib, "glc2d_win32_debug.lib")
+#endif
+#else
+#if defined(_M_X64)
+#pragma comment(lib, "glc2d_x64_release.lib")
+#elif defined(_M_IX86)
+#pragma comment(lib, "glc2d_win32_release.lib")
+#endif
+#endif
 
 int main()
 {
-	std::cout << "Hello World!!";
+    // SDK 초기화
+    g2_InitSdk();
+
+    printf("Starting ...\n\n");
+
+    // 윈도우 생성
+    g2_CreateWin(100, 100, 1024, 600, "Hello World");
+
+    // 실행
+    g2_Run();
+
+    // 윈도우 해제
+    g2_DestroyWin();
 }
